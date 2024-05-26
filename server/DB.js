cf3932332c2d235416f3602402f4e0f4ebd295ba
mohammadmikkawi <152801363+mohammadmikkawi@users.noreply.github.com>
2024-05-26 21:55:53 +0300

    About Us Page (Full Stack).
    About Us Page (Full Stack).

--- server/DB.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const uri = 'mongodb://localhost:27017/mohammaddb'; 
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
-
-module.exports = mongoose;
